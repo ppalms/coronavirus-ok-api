4adbda8b1e85a04de0ea2ef074a3b369912baf86
Patrick Palmer <ppalmer@pm.me>
2020-03-30 09:06:46 -0500

    Remove unused endpoints and add statewide endpoints

---
+++ casesStatewideWorker.js
@@ -0,0 +1,23 @@
+import * as scraper from './scrapeResults';
+import * as statewideResults from './listCasesStatewide';
+import * as persist from './persistResult';
+import moment from 'moment';
+
+export async function main() {
+  const today = moment().utc().format('L'); // 03/26/2020
+  const result = await statewideResults.byDate({ pathParameters: JSON.stringify(today) });
+  const existing = JSON.parse(result.body);
+
+  if (existing.length > 0) {
+    console.log(`Already found results for ${today}`);
+    return;
+  }
+
+  const results = await scraper.main();
+  console.log(`Found ${results.length} new results for ${today}`);
+
+  for (const result of results) {
+    const event = { body: JSON.stringify(result) };
+    await persist.main(event);
+  }
+}
